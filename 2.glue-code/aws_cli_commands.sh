e8c6fb79f06819f0244be7dfc584d8625428f787
Colton Robbins <coltonrobbins73@gmail.com>
2024-09-08 21:29:06 -0700

    trying to make a generalized script for etl

--- 2.glue-code/aws_cli_commands.sh
+++ 2.glue-code/aws_cli_commands.sh
@@ -2,6 +2,10 @@
 This is a list of cli commands for easy reference
 END_COMMENT
 
+# make a database from json and verify
+aws glue create-database --database-input file://make_rawdata_db.json
+aws glue get-database --name processed_data
+
 # make a table from json and check that it exists
 aws glue create-table --database-name raw_data --table-input file://raw_schema.json
 aws glue get-table --database-name raw_data --name customers
@@ -10,6 +14,9 @@ aws glue get-table --database-name raw_data --name customers
 aws glue create-crawler --cli-input-json file://mk_emp_crawler.json
 aws glue start-crawler --name "employees"
 aws glue get-crawler --name "employees"
+
+# save etl script to s3 bucket
+aws s3 cp etl_job.py s3://data-engineering-course-2024-abc123xyz/scriptLocation/etl_job.py
 
 # make etl job and run it
 aws glue create-job --cli-input-json file://customer_etl_job.json
--- 2.glue-code/etl_job.py
+++ 2.glue-code/etl_job.py
@@ -4,47 +4,52 @@ from awsglue.utils import getResolvedOptions
 from pyspark.context import SparkContext
 from awsglue.context import GlueContext
 from awsglue.job import Job
-from awsglue.dynamicframe import DynamicFrame
-from pyspark.sql.functions import current_timestamp
 
-# Initialize contexts and session
+# Parse job arguments
+args = getResolvedOptions(sys.argv, [
+  'JOB_NAME',
+  'source_database',
+  'source_table',
+  'target_path',
+  'target_database',
+  'target_table'
+])
+
+# Initialize Spark and Glue contexts
 sc = SparkContext()
 glueContext = GlueContext(sc)
 spark = glueContext.spark_session
+
+# Initialize Glue job
 job = Job(glueContext)
+job.init(args['JOB_NAME'], args)
 
-# Parameters
-args = getResolvedOptions(sys.argv, ['JOB_NAME', 'source_database', 'source_table', 'target_path', 'target_database', 'target_table'])
-
-source_database = args['source_database']
-source_table = args['source_table']
-target_path = args['target_path']
-target_database = args['target_database']
-target_table = args['target_table']
-
-# Read from Glue Catalog
+# Extract data from AWS Glue Data Catalog
 dynamic_frame = glueContext.create_dynamic_frame.from_catalog(
-  database=source_database,
-  table_name=source_table
+  database=args['source_database'], 
+  table_name=args['source_table'], 
+  transformation_ctx="dynamic_frame"
 )
 
-# Convert to DataFrame to add loadtime
-df = dynamic_frame.toDF()
-df = df.withColumn("loadtime", current_timestamp())
+# Add current timestamp to the data
+dynamic_frame_with_timestamp = dynamic_frame.gs_now(colName="loadtime")
 
-# Write to S3 in Parquet format, partitioned by loadtime
-df.write.partitionBy("loadtime").mode("append").parquet(target_path)
-
-# Create or update table in Glue Catalog
-glueContext.write_dynamic_frame.from_options(
-  frame=DynamicFrame.fromDF(df, glueContext, "nested"),
-  connection_type="s3",
-  connection_options={"path": target_path},
-  format="parquet",
-  transformation_ctx="datasink",
-  format_options={"partitionKeys": ["loadtime"]},
-  database=target_database,
-  table_name=target_table
+# Define the output sink
+sink = glueContext.getSink(
+  path=args['target_path'], 
+  connection_type="s3", 
+  updateBehavior="UPDATE_IN_DATABASE", 
+  partitionKeys=["loadtime"], 
+  enableUpdateCatalog=True, 
+  transformation_ctx="sink"
 )
 
+# Set catalog information and format for the output
+sink.setCatalogInfo(catalogDatabase=args['target_database'], catalogTableName=args['target_table'])
+sink.setFormat("glueparquet", compression="snappy")
+
+# Write the transformed data to the sink
+sink.writeFrame(dynamic_frame_with_timestamp)
+
+# Commit the job
 job.commit()
